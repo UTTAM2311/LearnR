6dc32a4d5a12c0603d008112be131f8a2c8572ed
surenraju <suren.r@imaginea.com>
2013-08-22 13:17:53 +0530

    Using message utils for mahout properties

--- src/main/java/com/imaginea/dc/mahout/model/MahoutBinaryClassifierModelBuilder.java
+++ src/main/java/com/imaginea/dc/mahout/model/MahoutBinaryClassifierModelBuilder.java
@@ -1,9 +1,5 @@
 package com.imaginea.dc.mahout.model;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +8,7 @@ import com.imaginea.dc.mahout.model.buider.ModelBuilderException;
 import com.imaginea.dc.mahout.model.buider.TableInputBinaryNBClassifierModelBuilder;
 import com.imaginea.dc.service.NewsArticleService;
 import com.imaginea.dc.service.impl.NewsArticleServiceImpl;
+import com.imaginea.dc.utils.MessageUtil;
 
 public class MahoutBinaryClassifierModelBuilder {
 	
@@ -20,34 +17,27 @@ public class MahoutBinaryClassifierModelBuilder {
 	private NewsArticleService newsArticleService;
 	
 	public void buildInputBinaryClassifierModel() {
-		Properties prop = new Properties();
-
 		try {
-			// load a properties file
-			prop.load(new FileInputStream("\\git\\DeathCluster\\src\\main\\resources\\mahout.properties"));
 
 			// get the property value and print it out
 			//String hdfsName = prop.getProperty("hdfs.name");
 			//String hdfsURI = prop.getProperty("hdfs.uri");
 			//String hdfsBaseDir = prop.getProperty("hdfs.base.dir");
+			String localBaseDir = MessageUtil.getMessage("local.base.dir");
 			
-			String localBaseDir = prop.getProperty("local.base.dir");
-			
-			String localInputSeqFileName = localBaseDir + prop.getProperty("model1.local.input.sequence.filename");
+			String localInputSeqFileName = localBaseDir + MessageUtil.getMessage("model1.local.input.sequence.filename");
 
-			String localVectorOutputFileDir = localBaseDir + prop.getProperty("model1.local.output.vector.filedir");
+			String localVectorOutputFileDir = localBaseDir + MessageUtil.getMessage("model1.local.output.vector.filedir");
 
-			String localModelOutputFileDir = localBaseDir + prop.getProperty("model1.local.output.model.filedir");
+			String localModelOutputFileDir = localBaseDir + MessageUtil.getMessage("model1.local.output.model.filedir");
 			
-			String luceneAnalyser = prop.getProperty("model1.lucene.analyser");
+			String luceneAnalyser = MessageUtil.getMessage("model1.lucene.analyser");
 			
 			MahoutClassifierModelBuilder template = new TableInputBinaryNBClassifierModelBuilder(newsArticleService, 
 					localInputSeqFileName, localVectorOutputFileDir, localModelOutputFileDir, luceneAnalyser);
 			
 			template.buildModel();
 
-		} catch (IOException ex) {
-			LOGGER.error("Error building the model", ex);
 		} catch (ModelBuilderException e) {
 			LOGGER.error("Error building the model", e);
 		}
