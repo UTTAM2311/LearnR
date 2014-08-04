bdfeb5b7e8ad0b754d6c004cc28d8675ef33ce82
milli <gowrishankar.milli@gmail.com>
2014-08-04 14:21:38 +0530

    core : added learnr-hadoop Module

---
+++ java/learnr-hadoop/src/main/java/org/learnr/hadoop/App.java
@@ -0,0 +1,13 @@
+package org.learnr.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
